2297ee4a498ba865d2c6bb5a2ec76d7a5e18b4e0
Brian Liao <yaramiu@gmail.com>
2022-09-06 17:30:20 -0400

    Implement playRound

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -4,3 +4,33 @@ function getComputerChoice() {
   let choice = choices[randomArrayIndex];
   return choice;
 }
+
+function playRound(playerSelection, computerSelection) {
+  let gameMessage = "";
+  playerSelection = playerSelection.toLowerCase();
+
+  if (playerSelection === "rock" && computerSelection === "rock") {
+    gameMessage = "It's a Tie! Both players chose Rock";
+  } else if (playerSelection === "rock" && computerSelection === "paper") {
+    gameMessage = "You Lose! Paper beats Rock";
+  } else if (playerSelection === "rock" && computerSelection === "scissors") {
+    gameMessage = "You Win! Rock beats Scissors";
+  } else if (playerSelection === "paper" && computerSelection === "rock") {
+    gameMessage = "You Win! Paper beats Rock";
+  } else if (playerSelection === "paper" && computerSelection === "paper") {
+    gameMessage = "It's a Tie! Both players chose Paper";
+  } else if (playerSelection === "paper" && computerSelection === "scissors") {
+    gameMessage = "You Lose! Scissors beats Paper";
+  } else if (playerSelection === "scissors" && computerSelection === "rock") {
+    gameMessage = "You Lose! Rock beats Scissors";
+  } else if (playerSelection === "scissors" && computerSelection === "paper") {
+    gameMessage = "You Win! Scissors beats Paper";
+  } else if (
+    playerSelection === "scissors" &&
+    computerSelection === "scissors"
+  ) {
+    gameMessage = "It's a Tie! Both players chose Scissors";
+  }
+
+  return gameMessage;
+}
