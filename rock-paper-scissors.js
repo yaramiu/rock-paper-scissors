2272d961691139a38d8a3acf83d5a233c64a9fb1
Brian Liao <yaramiu@gmail.com>
2022-09-07 11:34:19 -0400

    Implement game

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -6,31 +6,101 @@ function getComputerChoice() {
 }
 
 function playRound(playerSelection, computerSelection) {
-  let gameMessage = "";
+  let winner = "";
   playerSelection = playerSelection.toLowerCase();
 
   if (playerSelection === "rock" && computerSelection === "rock") {
-    gameMessage = "It's a Tie! Both players chose Rock";
+    console.log("It's a Tie! Both players chose Rock");
+    winner = "both";
   } else if (playerSelection === "rock" && computerSelection === "paper") {
-    gameMessage = "You Lose! Paper beats Rock";
+    console.log("You Lose! Paper beats Rock");
+    winner = "computer";
   } else if (playerSelection === "rock" && computerSelection === "scissors") {
-    gameMessage = "You Win! Rock beats Scissors";
+    console.log("You Win! Rock beats Scissors");
+    winner = "player";
   } else if (playerSelection === "paper" && computerSelection === "rock") {
-    gameMessage = "You Win! Paper beats Rock";
+    console.log("You Win! Paper beats Rock");
+    winner = "player";
   } else if (playerSelection === "paper" && computerSelection === "paper") {
-    gameMessage = "It's a Tie! Both players chose Paper";
+    console.log("It's a Tie! Both players chose Paper");
+    winner = "both";
   } else if (playerSelection === "paper" && computerSelection === "scissors") {
-    gameMessage = "You Lose! Scissors beats Paper";
+    console.log("You Lose! Scissors beats Paper");
+    winner = "computer";
   } else if (playerSelection === "scissors" && computerSelection === "rock") {
-    gameMessage = "You Lose! Rock beats Scissors";
+    console.log("You Lose! Rock beats Scissors");
+    winner = "computer";
   } else if (playerSelection === "scissors" && computerSelection === "paper") {
-    gameMessage = "You Win! Scissors beats Paper";
+    console.log("You Win! Scissors beats Paper");
+    winner = "player";
   } else if (
     playerSelection === "scissors" &&
     computerSelection === "scissors"
   ) {
-    gameMessage = "It's a Tie! Both players chose Scissors";
+    console.log("It's a Tie! Both players chose Scissors");
+    winner = "both";
   }
 
-  return gameMessage;
+  return winner;
 }
+
+function game() {
+  let playerScore = 0;
+  let computerScore = 0;
+  const ROUNDS_TO_PLAY = 5;
+
+  for (round = 1; round <= ROUNDS_TO_PLAY; round++) {
+    let playerSelection = prompt("Choose rock, paper, or scissors: ");
+
+    if (playerSelection === null) {
+      return;
+    }
+
+    let isValidSelection = checkValidSelection(playerSelection);
+    while (!isValidSelection) {
+      playerSelection = prompt(
+        "Invalid selection. Please choose only rock, paper, or scissors: "
+      );
+      isValidSelection = checkValidSelection(playerSelection);
+    }
+
+    let computerSelection = getComputerChoice();
+
+    let roundWinner = playRound(playerSelection, computerSelection);
+    if (roundWinner === "player") {
+      playerScore += 1;
+    } else if (roundWinner === "computer") {
+      computerScore += 1;
+    } else {
+      playerScore += 0.5;
+      computerScore += 0.5;
+    }
+
+    console.log(
+      `Results after round ${round}: player: ${playerScore} - computer: ${computerScore}`
+    );
+  }
+
+  if (playerScore > computerScore) {
+    console.log("Player is the winner");
+  } else if (playerScore < computerScore) {
+    console.log("Computer is the winner");
+  } else {
+    console.log("It is a draw");
+  }
+}
+
+function checkValidSelection(playerSelection) {
+  if (playerSelection) {
+    playerSelection = playerSelection.toLowerCase();
+    let isValidSelection =
+      playerSelection === "rock" ||
+      playerSelection === "paper" ||
+      playerSelection === "scissors";
+    return isValidSelection;
+  } else {
+    return false;
+  }
+}
+
+game();
